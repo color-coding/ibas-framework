945fab61a4ea2a4a2a59300da73b179bf686de0c
牛加人等于朱 <niuren.zhu@icloud.com>
2016-07-08 20:24:03 +0800

    添加业务逻辑测试。

--- bobas.businessobjectscommon/src/test/java/org/colorcoding/ibas/bobas/test/logics/testLogics.java
+++ bobas.businessobjectscommon/src/test/java/org/colorcoding/ibas/bobas/test/logics/testLogics.java
@@ -25,7 +25,7 @@ public class testLogics extends TestCase {
 		materials02.setItemDescription("Disk 5T");
 		// 保存物料到文件系统
 		IBORepository4File fileRepository = new BORepository4File();
-		//fileRepository.setRepositoryFolder("D:\\WorkTemp\\borepository");
+		// fileRepository.setRepositoryFolder("D:\\WorkTemp\\borepository");
 		BORepositoryTest boRepository = new BORepositoryTest();
 		boRepository.setRepository(fileRepository);
 		IOperationResult<?> operationResult;
@@ -39,21 +39,37 @@ public class testLogics extends TestCase {
 		ISalesOrder order = new SalesOrder();
 		order.setCustomerCode("C00001");
 		order.setCustomerName("宇宙无敌影业");
-		ISalesOrderItem item = order.getSalesOrderItems().create();
-		item.setItemCode(materials01.getItemCode());
-		item.setItemDescription(materials01.getItemDescription());
-		item.setQuantity(1);
-		item.setPrice(999999.99);
-		item = order.getSalesOrderItems().create();
-		item.setItemCode(materials02.getItemCode());
-		item.setItemDescription(materials02.getItemDescription());
-		item.setQuantity(2);
-		item.setPrice(99.00);
+		ISalesOrderItem item01 = order.getSalesOrderItems().create();
+		item01.setItemCode(materials01.getItemCode());
+		item01.setItemDescription(materials01.getItemDescription());
+		item01.setQuantity(1);
+		item01.setPrice(999999.99);
+		ISalesOrderItem item02 = order.getSalesOrderItems().create();
+		item02.setItemCode(materials02.getItemCode());
+		item02.setItemDescription(materials02.getItemDescription());
+		item02.setQuantity(2);
+		item02.setPrice(99.00);
 
 		operationResult = boRepository.saveSalesOrder(order);
 		if (operationResult.getResultCode() != 0) {
 			System.err.println(operationResult.getMessage());
 		}
 		assertEquals(operationResult.getMessage(), operationResult.getResultCode(), 0);
+
+		operationResult = boRepository.fetchMaterials(materials01.getCriteria());
+		assertEquals(operationResult.getMessage(), operationResult.getResultCode(), 0);
+		IMaterials materials01s = (IMaterials) operationResult.getResultObjects().firstOrDefault();
+		// 检索的物料是否一致
+		assertEquals("materials not same.", materials01.getItemCode(), materials01s.getItemCode());
+		// 订购数量是否增加
+		assertEquals(String.format("wrong matrials [%s] order quantity.", materials01.getItemCode()),
+				materials01.getOnOrder().add(item01.getQuantity()), materials01s.getOnOrder());
+		operationResult = boRepository.fetchMaterials(materials02.getCriteria());
+		assertEquals(operationResult.getMessage(), operationResult.getResultCode(), 0);
+		IMaterials materials02s = (IMaterials) operationResult.getResultObjects().firstOrDefault();
+		assertEquals("materials not same.", materials02.getItemCode(), materials02s.getItemCode());
+		assertEquals(String.format("wrong matrials [%s] order quantity.", materials02.getItemCode()),
+				materials02.getOnOrder().add(item02.getQuantity()), materials02s.getOnOrder());
+
 	}
 }
