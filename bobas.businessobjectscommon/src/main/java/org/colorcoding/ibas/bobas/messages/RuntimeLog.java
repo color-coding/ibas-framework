9eac9448cc722e57780785fa34223808f9fa81c0
three-stones <binghuo311@126.com>
2016-10-21 14:00:18 +0800

    调整日志文件名格式可配置

--- bobas.businessobjectscommon/src/main/java/org/colorcoding/ibas/bobas/messages/RuntimeLog.java
+++ bobas.businessobjectscommon/src/main/java/org/colorcoding/ibas/bobas/messages/RuntimeLog.java
@@ -111,7 +111,7 @@ public class RuntimeLog {
 		if (recorder == null) {
 			synchronized (RuntimeLog.class) {
 				if (recorder == null) {
-					recorder = RecorderFactory.createRecorder("ibas_runtime_%s_%g.log");
+					recorder = RecorderFactory.createRecorder();
 				}
 			}
 		}
