4d3ca81f8b652ad37d3eaf8408f0802e665124ab
牛加人等于朱 <niuren.zhu@icloud.com>
2016-11-02 16:58:16 +0800

    业务逻辑提交时保存的被影响对象，不受审批控制。

--- bobas.businessobjectscommon/src/main/java/org/colorcoding/ibas/bobas/logics/BusinessLogicsRepository.java
+++ bobas.businessobjectscommon/src/main/java/org/colorcoding/ibas/bobas/logics/BusinessLogicsRepository.java
@@ -15,6 +15,7 @@ class BusinessLogicsRepository extends BORepositoryLogicService {
 	public BusinessLogicsRepository() {
 		this.setRefetchAfterSave(false);// 保存后不检索数据
 		this.setUseCache(false);// 不使用缓存数据
+		this.setCheckApprovalProcess(false);// 不检查审批逻辑
 	}
 
 	public IOperationResult<?> save(IBusinessObjectBase bo) {
