a4c76d307fe2b76bff85890a1e8cdb077d462ffc
牛加人等于朱 <niuren.zhu@icloud.com>
2016-12-23 10:44:07 +0800

    调整描述。

--- compile_and_package.bat
+++ compile_and_package.bat
@@ -1,17 +1,17 @@
 @echo off
 setlocal EnableDelayedExpansion
-echo *****************************************************************
+echo ***************************************************************************
 echo      compile_and_package.bat
 echo                     by niuren.zhu
 echo                           2016.06.19
 echo  说明：
-echo     1. 安装apache-maven，下载地址http://ibas.club:8866/ibas/basis/
-echo     2. 解压apache-maven，并设置系统变量%MAVEN_HOME%为解压的程序目录。
-echo     3. 添加PATH变量到%MAVEN_HOME%\bin，并检查JAVE_HOME配置是否正确。
+echo     1. 安装apache-maven，下载地址http://maven.apache.org/download.cgi。
+echo     2. 解压apache-maven，并设置系统变量MAVEN_HOME为解压的程序目录。
+echo     3. 添加PATH变量到%%MAVEN_HOME%%\bin，并检查JAVA_HOME配置是否正确。
 echo     4. 运行提示符运行mvn -v 检查安装是否成功。
 echo     5. 此脚本会遍历当前目录的子目录，查找pom.xml并编译jar包到release目录。
 echo     6. 可在compile_order.txt文件中调整编译顺序。
-echo *****************************************************************
+echo ****************************************************************************
 REM 设置参数变量
 SET WORK_FOLDER=%~dp0
 SET h=%time:~0,2%
