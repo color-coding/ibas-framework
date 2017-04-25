ba723874bc2508bd3cfe52e04f710ba5b9c85cbe
牛加人等于朱 <niuren.zhu@icloud.com>
2017-04-25 13:52:49 +0800

    调整序列化标记

--- bobas.businessobjectscommon/src/main/java/org/colorcoding/ibas/bobas/data/FileData.java
+++ bobas.businessobjectscommon/src/main/java/org/colorcoding/ibas/bobas/data/FileData.java
@@ -2,61 +2,73 @@ package org.colorcoding.ibas.bobas.data;
 
 import java.io.InputStream;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+
+import org.colorcoding.ibas.bobas.MyConsts;
+
 /**
  * 文件数据
  * 
  * @author Niuren.Zhu
  *
  */
+@XmlType(name = "FileData", namespace = MyConsts.NAMESPACE_BOBAS_DATA)
+@XmlRootElement(name = "FileData", namespace = MyConsts.NAMESPACE_BOBAS_DATA)
 public class FileData {
 
-    private String fileName;
+	private String fileName;
 
-    /**
-     * 文件名称
-     * 
-     * @return
-     */
-    public String getFileName() {
-        return fileName;
-    }
+	/**
+	 * 文件名称
+	 * 
+	 * @return
+	 */
+	@XmlElement(name = "FileName")
+	public String getFileName() {
+		return fileName;
+	}
 
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
 
-    private String location;
+	private String location;
 
-    /**
-     * 文件位置
-     * 
-     * @return
-     */
-    public String getLocation() {
-        return location;
-    }
+	/**
+	 * 文件位置
+	 * 
+	 * @return
+	 */
+	@XmlElement(name = "Location")
+	public String getLocation() {
+		return location;
+	}
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
-    private InputStream stream;
+	private InputStream stream;
 
-    /**
-     * 文件流
-     * 
-     * @return
-     */
-    public InputStream getStream() {
-        return stream;
-    }
+	/**
+	 * 文件流
+	 * 
+	 * @return
+	 */
+	@XmlTransient
+	public InputStream getStream() {
+		return stream;
+	}
 
-    public void setStream(InputStream stream) {
-        this.stream = stream;
-    }
+	public void setStream(InputStream stream) {
+		this.stream = stream;
+	}
 
-    @Override
-    public String toString() {
-        return String.format("{file data: %s}", this.getFileName());
-    }
+	@Override
+	public String toString() {
+		return String.format("{file data: %s}", this.getFileName());
+	}
 }
