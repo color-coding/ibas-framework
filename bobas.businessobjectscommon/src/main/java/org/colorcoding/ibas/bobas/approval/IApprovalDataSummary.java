0361bb23d958a06a751b6a93f3cc666225eb5102
牛加人等于朱 <niuren.zhu@icloud.com>
2017-05-11 17:30:11 +0800

    添加审批数据摘要接口

---
+++ bobas.businessobjectscommon/src/main/java/org/colorcoding/ibas/bobas/approval/IApprovalDataSummary.java
@@ -0,0 +1,16 @@
+package org.colorcoding.ibas.bobas.approval;
+
+/**
+ * 审批数据摘要
+ * 
+ * @author Niuren.Zhu
+ *
+ */
+public interface IApprovalDataSummary {
+	/**
+	 * 获取审批数据摘要（注意长度限制）
+	 * 
+	 * @return
+	 */
+	String getSummary();
+}
