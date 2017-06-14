2bd1a7b1ad4831b6128d8a3640d44a60ee26ae4c
牛加人等于朱 <niuren.zhu@icloud.com>
2017-06-14 13:03:52 +0800

    添加守护线程结束方法。

--- bobas.businessobjectscommon/src/main/java/org/colorcoding/ibas/bobas/core/Daemon.java
+++ bobas.businessobjectscommon/src/main/java/org/colorcoding/ibas/bobas/core/Daemon.java
@@ -20,190 +20,221 @@ import org.colorcoding.ibas.bobas.messages.RuntimeLog;
  *
  */
 public class Daemon implements IDaemon {
-    /**
-     * 注册后台任务
-     * 
-     * @param task
-     *            任务
-     * @return 任务ID，小于0任务注册失败
-     * @throws InvalidDaemonTask
-     */
-    public static long register(IDaemonTask task) throws InvalidDaemonTask {
-        return register(task, true);
-    }
+	/**
+	 * 注册后台任务
+	 * 
+	 * @param task
+	 *            任务
+	 * @return 任务ID，小于0任务注册失败
+	 * @throws InvalidDaemonTask
+	 */
+	public static long register(IDaemonTask task) throws InvalidDaemonTask {
+		return register(task, true);
+	}
 
-    /**
-     * 注册后台任务
-     * 
-     * @param task
-     *            任务
-     * @param log
-     *            是否记录日志
-     * @return 任务ID，小于0任务注册失败
-     * @throws InvalidDaemonTask
-     */
-    public static long register(IDaemonTask task, boolean log) throws InvalidDaemonTask {
-        synchronized (Daemon.class) {
-            return create().add(task, log);
-        }
-    }
+	/**
+	 * 注册后台任务
+	 * 
+	 * @param task
+	 *            任务
+	 * @param log
+	 *            是否记录日志
+	 * @return 任务ID，小于0任务注册失败
+	 * @throws InvalidDaemonTask
+	 */
+	public static long register(IDaemonTask task, boolean log) throws InvalidDaemonTask {
+		synchronized (Daemon.class) {
+			return create().add(task, log);
+		}
+	}
 
-    /**
-     * 移出任务
-     * 
-     * @param id
-     *            注册时分配的id
-     * @return true，成功；false，失败
-     */
-    public static boolean unRegister(long id) {
-        synchronized (Daemon.class) {
-            return create().remove(id);
-        }
-    }
+	/**
+	 * 终止线程
+	 */
+	public static void destory() {
+		synchronized (Daemon.class) {
+			if (daemon != null) {
+				daemon.stop();
+				daemon = null;
+				System.gc();
+			}
+		}
+	}
 
-    private volatile static IDaemon daemon;
+	/**
+	 * 移出任务
+	 * 
+	 * @param id
+	 *            注册时分配的id
+	 * @return true，成功；false，失败
+	 */
+	public static boolean unRegister(long id) {
+		synchronized (Daemon.class) {
+			return create().remove(id);
+		}
+	}
 
-    static IDaemon create() {
-        if (daemon == null) {
-            synchronized (Daemon.class) {
-                if (daemon == null) {
-                    daemon = new Daemon();
-                    daemon.initialize();
-                }
-            }
-        }
-        return daemon;
-    }
+	private volatile static IDaemon daemon;
 
-    private volatile Thread daemonThread;
+	static IDaemon create() {
+		if (daemon == null) {
+			synchronized (Daemon.class) {
+				if (daemon == null) {
+					daemon = new Daemon();
+					daemon.initialize();
+				}
+			}
+		}
+		return daemon;
+	}
 
-    @Override
-    public synchronized void initialize() {
-        if (this.daemonThread == null) {
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    while (true) {
-                        checkRun();
-                        try {
-                            Thread.sleep(500);// 每500毫秒检查次任务
-                        } catch (Exception e) {
-                            RuntimeLog.log(e);
-                        }
-                    }
-                }
-            });
-            thread.setName("ibas-framework-daemon");
-            thread.setPriority(Thread.MAX_PRIORITY);
-            thread.setDaemon(true);
-            thread.start();
-        }
-    }
+	private volatile Thread daemonThread;
 
-    private ArrayList<DaemonTaskWrapping> wrappings;
+	@Override
+	public synchronized void initialize() {
+		this.running = true;
+		if (this.daemonThread == null) {
+			Thread thread = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					while (running) {
+						checkRun();
+						try {
+							Thread.sleep(500);// 每500毫秒检查次任务
+						} catch (Exception e) {
+							RuntimeLog.log(e);
+						}
+					}
+				}
+			});
+			thread.setName("ibas-framework-daemon");
+			thread.setPriority(Thread.MAX_PRIORITY);
+			thread.setDaemon(true);
+			thread.start();
+		}
+	}
 
-    protected ArrayList<DaemonTaskWrapping> getWrappings() {
-        if (wrappings == null) {
-            wrappings = new ArrayList<>();
-        }
-        return wrappings;
-    }
+	/**
+	 * 运行中标记
+	 */
+	private volatile boolean running;
 
-    @Override
-    public long add(IDaemonTask task, boolean isLog) throws InvalidDaemonTask {
-        if (task == null || task.getName() == null || task.getName().isEmpty()) {
-            throw new InvalidDaemonTask();
-        }
-        if (task instanceof ISingleDaemonTask) {
-            ISingleDaemonTask singleTask = (ISingleDaemonTask) task;
-            if (singleTask.getKeepTime() < 1 || singleTask.getLockSignature() == null
-                    || singleTask.getLockSignature().isEmpty()) {
-                throw new InvalidDaemonTask();
-            }
-        }
-        synchronized (this.getWrappings()) {
-            DaemonTaskWrapping wrapping = new DaemonTaskWrapping(task);
-            wrapping.setLog(isLog);
-            wrapping.setId(Math.abs(UUID.randomUUID().getLeastSignificantBits()));
-            this.getWrappings().add(wrapping);
-            RuntimeLog.log(RuntimeLog.MSG_DAEMON_REGISTER_TASK, wrapping.getId(), wrapping.getName());
-            return wrapping.getId();
-        }
-    }
+	@Override
+	public void stop() {
+		this.running = false;
+		if (this.daemonThread != null) {
+			this.daemonThread.interrupt();
+		}
+		if (this.threadPool != null) {
+			this.threadPool.shutdown();
+		}
+	}
 
-    @Override
-    public boolean remove(long taskId) {
-        if (taskId < 0) {
-            return false;
-        }
-        synchronized (this.getWrappings()) {
-            for (int i = this.getWrappings().size() - 1; i >= 0; i--) {
-                DaemonTaskWrapping wrapping = this.getWrappings().get(i);
-                if (wrapping == null) {
-                    continue;
-                }
-                if (wrapping.getId() == taskId) {
-                    this.getWrappings().remove(i);
-                }
-                RuntimeLog.log(RuntimeLog.MSG_DAEMON_REMOVE_TASK, wrapping.getId(), wrapping.getName());
-            }
-        }
-        return false;
-    }
+	private ArrayList<DaemonTaskWrapping> wrappings;
 
-    ExecutorService threadPool;
+	protected ArrayList<DaemonTaskWrapping> getWrappings() {
+		if (wrappings == null) {
+			wrappings = new ArrayList<>();
+		}
+		return wrappings;
+	}
 
-    public ExecutorService getThreadPool() {
-        if (this.threadPool == null) {
-            int cpu = Runtime.getRuntime().availableProcessors();
-            int size = MyConfiguration.getConfigValue(MyConfiguration.CONFIG_ITEM_TASK_THREAD_POOL_SIZE, cpu);
-            // this.threadPool = Executors.newFixedThreadPool(size);
-            this.threadPool = new ThreadPoolExecutor(1, size, 1000L, TimeUnit.MILLISECONDS,
-                    new LinkedBlockingQueue<Runnable>());
-        }
-        return threadPool;
-    }
+	@Override
+	public long add(IDaemonTask task, boolean isLog) throws InvalidDaemonTask {
+		if (task == null || task.getName() == null || task.getName().isEmpty()) {
+			throw new InvalidDaemonTask();
+		}
+		if (task instanceof ISingleDaemonTask) {
+			ISingleDaemonTask singleTask = (ISingleDaemonTask) task;
+			if (singleTask.getKeepTime() < 1 || singleTask.getLockSignature() == null
+					|| singleTask.getLockSignature().isEmpty()) {
+				throw new InvalidDaemonTask();
+			}
+		}
+		synchronized (this.getWrappings()) {
+			DaemonTaskWrapping wrapping = new DaemonTaskWrapping(task);
+			wrapping.setLog(isLog);
+			wrapping.setId(Math.abs(UUID.randomUUID().getLeastSignificantBits()));
+			this.getWrappings().add(wrapping);
+			RuntimeLog.log(RuntimeLog.MSG_DAEMON_REGISTER_TASK, wrapping.getId(), wrapping.getName());
+			return wrapping.getId();
+		}
+	}
 
-    @Override
-    public void checkRun() {
-        long time = System.currentTimeMillis();
-        synchronized (this.getWrappings()) {
-            for (DaemonTaskWrapping wrapping : this.getWrappings()) {
-                if (wrapping == null) {
-                    continue;
-                }
-                if (wrapping.isRunning()) {
-                    // 同时仅启动一个任务
-                    continue;
-                }
-                if (!wrapping.isActivated()) {
-                    continue;
-                }
-                boolean done = wrapping.tryRun(time);
-                if (done) {
-                    // 可以运行
-                    wrapping.setRunning(true);// 设置状态为运行中
-                    long start = System.currentTimeMillis();
-                    long times = wrapping.getRunTimes() + 1;
-                    if (wrapping.isLog()) {
-                        RuntimeLog.log(MessageLevel.DEBUG, RuntimeLog.MSG_DAEMON_TASK_START, wrapping.getId(),
-                                wrapping.getName(), times);
-                    }
-                    // 从线程池中调用新的线程运行此任务
-                    this.getThreadPool().execute(new Runnable() {
+	@Override
+	public boolean remove(long taskId) {
+		if (taskId < 0) {
+			return false;
+		}
+		synchronized (this.getWrappings()) {
+			for (int i = this.getWrappings().size() - 1; i >= 0; i--) {
+				DaemonTaskWrapping wrapping = this.getWrappings().get(i);
+				if (wrapping == null) {
+					continue;
+				}
+				if (wrapping.getId() == taskId) {
+					this.getWrappings().remove(i);
+				}
+				RuntimeLog.log(RuntimeLog.MSG_DAEMON_REMOVE_TASK, wrapping.getId(), wrapping.getName());
+			}
+		}
+		return false;
+	}
 
-                        @Override
-                        public void run() {
-                            wrapping.run();
-                            long end = System.currentTimeMillis();
-                            if (wrapping.isLog()) {
-                                RuntimeLog.log(MessageLevel.DEBUG, RuntimeLog.MSG_DAEMON_TASK_COMPLETED,
-                                        wrapping.getId(), wrapping.getName(), times, (end - start));
-                            }
-                        }
-                    });
-                }
-            }
-        }
-    }
+	ExecutorService threadPool;
+
+	public ExecutorService getThreadPool() {
+		if (this.threadPool == null) {
+			int cpu = Runtime.getRuntime().availableProcessors();
+			int size = MyConfiguration.getConfigValue(MyConfiguration.CONFIG_ITEM_TASK_THREAD_POOL_SIZE, cpu);
+			// this.threadPool = Executors.newFixedThreadPool(size);
+			this.threadPool = new ThreadPoolExecutor(1, size, 1000L, TimeUnit.MILLISECONDS,
+					new LinkedBlockingQueue<Runnable>());
+		}
+		return threadPool;
+	}
+
+	@Override
+	public void checkRun() {
+		long time = System.currentTimeMillis();
+		synchronized (this.getWrappings()) {
+			for (DaemonTaskWrapping wrapping : this.getWrappings()) {
+				if (wrapping == null) {
+					continue;
+				}
+				if (wrapping.isRunning()) {
+					// 同时仅启动一个任务
+					continue;
+				}
+				if (!wrapping.isActivated()) {
+					continue;
+				}
+				boolean done = wrapping.tryRun(time);
+				if (done) {
+					// 可以运行
+					wrapping.setRunning(true);// 设置状态为运行中
+					long start = System.currentTimeMillis();
+					long times = wrapping.getRunTimes() + 1;
+					if (wrapping.isLog()) {
+						RuntimeLog.log(MessageLevel.DEBUG, RuntimeLog.MSG_DAEMON_TASK_START, wrapping.getId(),
+								wrapping.getName(), times);
+					}
+					// 从线程池中调用新的线程运行此任务
+					this.getThreadPool().execute(new Runnable() {
+
+						@Override
+						public void run() {
+							wrapping.run();
+							long end = System.currentTimeMillis();
+							if (wrapping.isLog()) {
+								RuntimeLog.log(MessageLevel.DEBUG, RuntimeLog.MSG_DAEMON_TASK_COMPLETED,
+										wrapping.getId(), wrapping.getName(), times, (end - start));
+							}
+						}
+					});
+				}
+			}
+		}
+	}
+
 }
--- bobas.businessobjectscommon/src/main/java/org/colorcoding/ibas/bobas/core/IDaemon.java
+++ bobas.businessobjectscommon/src/main/java/org/colorcoding/ibas/bobas/core/IDaemon.java
@@ -7,34 +7,39 @@ package org.colorcoding.ibas.bobas.core;
  *
  */
 public interface IDaemon {
-    /**
-     * 初始化
-     */
-    void initialize();
+	/**
+	 * 初始化
+	 */
+	void initialize();
 
-    /**
-     * 添加任务
-     * 
-     * @param task
-     *            任务
-     * @param log
-     *            是否记录日志
-     * @return 分配的任务id
-     * @throws InvalidDaemonTask
-     */
-    long add(IDaemonTask task, boolean log) throws InvalidDaemonTask;
+	/**
+	 * 关闭
+	 */
+	void stop();
 
-    /**
-     * 移出任务
-     * 
-     * @param taskId
-     *            分配的任务id
-     * @return true，成功；false，失败
-     */
-    boolean remove(long taskId);
+	/**
+	 * 添加任务
+	 * 
+	 * @param task
+	 *            任务
+	 * @param log
+	 *            是否记录日志
+	 * @return 分配的任务id
+	 * @throws InvalidDaemonTask
+	 */
+	long add(IDaemonTask task, boolean log) throws InvalidDaemonTask;
 
-    /**
-     * 检查并运行
-     */
-    void checkRun();
+	/**
+	 * 移出任务
+	 * 
+	 * @param taskId
+	 *            分配的任务id
+	 * @return true，成功；false，失败
+	 */
+	boolean remove(long taskId);
+
+	/**
+	 * 检查并运行
+	 */
+	void checkRun();
 }
