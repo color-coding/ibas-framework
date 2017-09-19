7acc49987a86fc1a8059ec4f8007292ebe7cc111
牛加人等于朱 <niuren.zhu@icloud.com>
2017-09-19 12:44:29 +0800

    优化编译脚本

--- compile_packages.bat
+++ compile_packages.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 echo ***************************************************************************
-echo      compile_and_package.bat
+echo            compile_packages.bat
 echo                     by niuren.zhu
 echo                           2016.06.19
 echo  说明：
@@ -17,49 +17,38 @@ SET WORK_FOLDER=%~dp0
 SET h=%time:~0,2%
 SET hh=%h: =0%
 SET OPNAME=%date:~0,4%%date:~5,2%%date:~8,2%_%hh%%time:~3,2%%time:~6,2%
-SET LOGFILE=%WORK_FOLDER%compile_and_package_log_%OPNAME%.txt
+SET LOGFILE=%WORK_FOLDER%compile_packages_log_%OPNAME%.txt
 
 echo --当前工作的目录是[%WORK_FOLDER%]
 echo --检查编译顺序文件[compile_order.txt]
-if not exist %WORKFOLDER%compile_order.txt dir /a:d /b %WORKFOLDER% >%WORKFOLDER%compile_order.txt
+if not exist %WORK_FOLDER%compile_order.txt dir /a:d /b %WORK_FOLDER% >%WORK_FOLDER%compile_order.txt
 
-echo --清除项目的maven缓存
+echo --清除项目缓存
+if exist %WORK_FOLDER%release\ rd /s /q %WORK_FOLDER%release\
 if not exist %WORK_FOLDER%release md %WORK_FOLDER%release
-if exist %WORK_FOLDER%release\*.* del /f /s /q %WORK_FOLDER%release\*.*
-call "%MAVEN_HOME%\bin\mvn" clean install >>%LOGFILE%
+call "%MAVEN_HOME%\bin\mvn" clean install -f %WORK_FOLDER% >>%LOGFILE%
 
 echo --开始编译[compile_order.txt]内容
-for /f %%m in (%WORKFOLDER%compile_order.txt) do (
+for /f %%m in (%WORK_FOLDER%compile_order.txt) do (
   if exist %WORK_FOLDER%%%m\pom.xml (
-    REM 改变当前工作目录
-    cd /d %WORK_FOLDER%%%m >>%LOGFILE%
     SET MY_PACKAGES_FOLDER=%%m
     if !MY_PACKAGES_FOLDER:~-8!==.service (
-      REM 网站，编译war包，拷贝jar包并安装到本地
+      REM 网站，编译war包
       echo --开始编译[%%m]
-      call "%MAVEN_HOME%\bin\mvn" clean package -Dmaven.test.skip=true >>%LOGFILE%
+      call "%MAVEN_HOME%\bin\mvn" clean package -Dmaven.test.skip=true -f %WORK_FOLDER%%%m >>%LOGFILE%
       if exist %WORK_FOLDER%%%m\target\%%m*.war copy /y %WORK_FOLDER%%%m\target\%%m*.war %WORK_FOLDER%release >>%LOGFILE%
-      REM 拷贝jar包并安装到本地
-      for /f %%l in ('dir /s /a /b %WORK_FOLDER%%%m\target\%%m*.jar' ) do (
-        copy /y %%l %WORK_FOLDER%release >>%LOGFILE%
-      )
     ) else (
       REM 非网站，编译jar包并安装到本地
       echo --开始编译[%%m]+安装
-      call "%MAVEN_HOME%\bin\mvn" clean package install -Dmaven.test.skip=true >>%LOGFILE%
+      call "%MAVEN_HOME%\bin\mvn" clean package install -Dmaven.test.skip=true -f %WORK_FOLDER%%%m >>%LOGFILE%
       if exist %WORK_FOLDER%%%m\target\%%m*.jar copy /y %WORK_FOLDER%%%m\target\%%m*.jar %WORK_FOLDER%release >>%LOGFILE%
     )
     REM 检查并复制编译结果
-    
     if exist %WORK_FOLDER%release\%%m*.* (
       echo --编译[%%m]成功
     ) else (
       echo --编译[%%m]失败
     )
   )
-    
-  REM 重置当前工作目录
-  cd /d %WORK_FOLDER%
 )
-
-echo --编译完成，更多信息请查看[compile_and_package_log_%OPNAME%.txt]
+echo --编译完成，更多信息请查看[compile_packages_log_%OPNAME%.txt]
