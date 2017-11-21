dd9ea3fc94e41c7357078a54bcb66cab19090200
牛加人等于朱 <niuren.zhu@icloud.com>
2017-11-21 11:37:18 +0800

    调整脚本解释程序

--- compile_packages.sh
+++ compile_packages.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 echo '****************************************************************************'
 echo '             compile_packages.sh                                            '
 echo '                      by niuren.zhu                                         '
--- deploy_packages.sh
+++ deploy_packages.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 echo '*****************************************************************'
 echo '              deploy_packages.sh                                 '
 echo '                    by niuren.zhu                                '
