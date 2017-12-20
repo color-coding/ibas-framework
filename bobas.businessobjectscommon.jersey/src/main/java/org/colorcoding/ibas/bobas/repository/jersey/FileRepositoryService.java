b925f4b55c71dad40319da8afe3ea409ed716a61
牛加人等于朱 <niuren.zhu@icloud.com>
2017-12-20 12:13:07 +0800

    优化资源文件下载

--- bobas.businessobjectscommon.jersey/src/main/java/org/colorcoding/ibas/bobas/repository/jersey/FileRepositoryService.java
+++ bobas.businessobjectscommon.jersey/src/main/java/org/colorcoding/ibas/bobas/repository/jersey/FileRepositoryService.java
@@ -1,7 +1,10 @@
 package org.colorcoding.ibas.bobas.repository.jersey;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.colorcoding.ibas.bobas.common.OperationResult;
 import org.colorcoding.ibas.bobas.data.FileData;
@@ -18,6 +21,18 @@ public class FileRepositoryService extends org.colorcoding.ibas.bobas.repository
 			return new OperationResult<>(super.save(fileData, token));
 		} catch (Exception e) {
 			return new OperationResult<>(e);
+		}
+	}
+
+	protected String getContentType(FileData fileData) {
+		return this.getContentType(fileData.getLocation());
+	}
+
+	protected String getContentType(String filePath) {
+		try {
+			return Files.probeContentType(Paths.get(filePath));
+		} catch (IOException e) {
+			return null;
 		}
 	}
 }
